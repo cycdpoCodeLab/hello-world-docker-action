8c68a102261f1a37da0c0ffe415533e4fa64b532
cycjimmy <cycjimmy@gmail.com>
2019-10-17 13:21:52 +0800

    chore: log

--- entrypoint.js
+++ entrypoint.js
@@ -1,15 +1,31 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 const core = require('@actions/core');
-const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+const sayHello = require('./src/index');
+
+const run = async ()=> {
+  {
+    // Log node version
+    const { stdout, stderr } = await exec('node --version');
+
+    console.log('node:', stdout);
+    if(stderr) {
+      return Promise.reject(stderr);
+    }
+  }
+
+  {
+    // Log npm version
+    const { stdout, stderr } = await exec('npm --version');
+
+    console.log('npm:', stdout);
+    if(stderr) {
+      return Promise.reject(stderr);
+    }
+  }
+
+  sayHello();
+};
+
+run().catch(core.setFailed);
